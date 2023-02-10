f50eafe8f44f6fc820cc84e7505597b0f10b4b60
Mahdi Adib <mahdiadib2001@gmail.com>
2023-02-10 22:26:20 +0600

    changes saved

--- Pages/Management/ManageProductTypes.aspx.cs
+++ Pages/Management/ManageProductTypes.aspx.cs
@@ -10,20 +10,18 @@ public partial class Pages_ManageProductTypes : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         //txtName.Text = "";
-        lblResult.Text = "";
+        //lblResult.Text = "";
     }
 
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
-        if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrWhiteSpace(txtName.Text)) lblResult.Text = "Enter a valid ProductType";
-        else
-        {
-            ProductTypeTypeModel model = new ProductTypeTypeModel();
-            ProductType pt = createProductType();
-            pt.Name = txtName.Text;
-            lblResult.Text = model.InsertProductType(pt);
-            //lblResult.Text = txtName.Text;
-        }
+        ProductTypeTypeModel model = new ProductTypeTypeModel();
+        ProductType pt = createProductType();
+        pt.Name = txtName.Text;
+        //lblResult.Text = model.InsertProductType(pt);
+        lblResult.Text = txtName.Text;
+        //Sidekick.Alert(txtName.Text, this);
+        txtName.Text = "";
     }
 
     ProductType createProductType()
